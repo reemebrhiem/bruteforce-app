ad39604574e8e43cf0e6374ac02dd1135a8fdb2c
reemebrhiem <158289228+reemebrhiem@users.noreply.github.com>
2026-01-29 18:30:22 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from datetime import datetime, timedelta
 import joblib
 import os
 
+print("DB URL FOUND:", bool(os.environ.get("DATABASE_URL")))
+
 app = Flask(__name__)
 app.secret_key = "secret123"
 
@@ -110,3 +112,4 @@ def dashboard(username):
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
+
