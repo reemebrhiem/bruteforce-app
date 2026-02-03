789e6f443bd519a1b7e677e60c532d2eb46cafc9
reemebrhiem <158289228+reemebrhiem@users.noreply.github.com>
2026-02-03 17:28:15 +0300

    Update app.py

--- app.py
+++ app.py
@@ -133,8 +133,9 @@ def login():
 
 @app.route("/dashboard/<username>")
 def dashboard(username):
-return render_template("dashboard.html", username=username)
+    return render_template("dashboard.html", username=username)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
+
